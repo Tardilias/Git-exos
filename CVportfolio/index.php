48ef8d65e16dab0e38892896259f395889fb9f29
Your Name <foo@bar.be>
2021-01-10 15:31:03 +0100

    CVportfolio

---
+++ CVportfolio/index.php
@@ -0,0 +1,4 @@
+<?php include_once("/Users/halyas/Desktop/GITHUB/Git-exos/CVportfolio/contact.html");
+include_once("/Users/halyas/Desktop/GITHUB/Git-exos/CVportfolio/index.html");
+include_once("/Users/halyas/Desktop/GITHUB/Git-exos/CVportfolio/mesprojets.html");
+include_once("/Users/halyas/Desktop/GITHUB/Git-exos/CVportfolio/moncv.html"); ?>
