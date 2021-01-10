cced3372325cf11b7a63d1d958126b2da4040a01
Your Name <foo@bar.be>
2021-01-10 16:17:06 +0100

    CVportfolio

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once("html/contact.html");
-include_once("html/index.html");
-include_once("html/mesprojets.html");
-include_once("html/moncv.html"); ?>
+<?php include_once("html/contact.html");?>
+<?php include_once("html/index.html");?>
+<?php include_once("html/mesprojets.html");?>
+<?php include_once("html/moncv.html"); ?>
